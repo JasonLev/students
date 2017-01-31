475ad3085b11de12635b29cb910fef60671cf752
JasonLev <jasond.levy@gmail.com>
2017-01-31 11:19:30 -0800

    add Week 9 content; add calculator.js in the hw folder

---
+++ code_of_hanzo/hw/calculator.js
@@ -0,0 +1,89 @@
+// Complete the following functions:
+
+// 1.  Square Root:
+// this function should take an input (a number) and output its square Root;
+// You are given almost the complete function, just provide a way to get the ouput (hint: return) of the function
+function squareRoot(n) {
+  var result = Math.sqrt(n);
+  //Your code goes on the line below here
+
+  //Your code goes on the line above
+}
+
+// Test the squareRoot function with this:
+squareRoot(144); //should output 12
+
+// ***********************************
+// 2.  Addition:
+// this function should take two inputs (two numbers) and output the sum of them;
+// You are given almost the complete function, just provide a way to get the ouput (hint: return) of the function
+function addition(x,y) {
+  var sum = x + y;
+  //Your code goes on the line below here
+
+  //Your code goes on the line above
+}
+
+// Test the addition function with this:
+addition(38, 21); //should output 59
+
+// ***********************************
+// 3.  Subtraction:
+// this function should take two inputs (two numbers) and output the difference of them (first input subtract the second input);
+// You are given a little less this time, you should complete creating a variable to compute the math and then provide a way to get the ouput (hint: return) of the function
+function subtraction(x,y) {
+  //Your code goes on the lines below here
+  var difference =            // <-- do the math here
+
+  //Your code goes on the lines above
+}
+
+// Test the subtraction function with this:
+subtraction(38, 21); //should output 59
+
+// ***********************************
+// 4.  Multiplication:
+// this function should take two inputs (two numbers) and output the product of them;
+// You are given even little less this time, you should create a variable to compute the math and then provide a way to get the ouput (hint: return) of the function
+function multiplication(x,y) {
+  //Your code goes on the lines below here
+
+
+  //Your code goes on the lines above
+}
+
+// Test the multiplication function with this:
+multiplication(10, 17); //should output 170
+
+// ***********************************
+// 5.  Division:
+// this function should take two inputs (two numbers) and output the quotient of them;
+// You should complete the whole thing by writing the entire function out...
+  //Your code goes on the lines below here
+
+
+
+
+  //Your code goes on the lines above
+
+// Test the division function with this:
+division(540, 18); //should output 30
+
+
+
+// ***********************************
+// Bonus:
+// create a function that is a "mash up math" of your other functions;
+// this function should take two inputs (two numbers) and compute a mathematical operation on the inputs (either addition, subtraction, multiplication, or division), then the function continues by computing another operation on the inputs (don't repeat, pick a different operation), then the function computes yet another operation (again, different from the first two operations) and uses the results of the first operations as inputs
+// You should complete the whole thing by writing the entire function out...
+  //Your code goes on the lines below here
+
+
+
+
+
+
+  //Your code goes on the lines above
+
+// Test the division function with this:
+mashUpMath(10, 4); //if you pick addition, subtraction, and multiplication... should output 84
