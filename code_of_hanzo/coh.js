44a908edaf43a148350c7f77b598f384bfad866c
JasonLev <jasond.levy@gmail.com>
2017-03-24 00:05:26 -0700

    button clicking js; styling to index

--- code_of_hanzo/coh.js
+++ code_of_hanzo/coh.js
@@ -1,6 +1,19 @@
 $(function() {
-  $("button").click(function() {
-    $("button").addClass('hide');
-    $(this).removeClass('hide');
+  $("#notes > button").click(function() {
+    $(".topics").addClass('hide');
+    var btnIndex = $(this).index('#notes > button');
+    $(".topics:eq("+btnIndex+")").removeClass('hide');
+  });
+
+  $("#hw > button").click(function() {
+    $(".homework").addClass('hide');
+    var btnIndex = $(this).index('#hw > button');
+    $(".homework:eq("+btnIndex+")").removeClass('hide');
+  });
+
+  $("#exercises > button").click(function() {
+    $(".exc").addClass('hide');
+    var btnIndex = $(this).index('#exercises > button');
+    $(".exc:eq("+btnIndex+")").removeClass('hide');
   });
 });
