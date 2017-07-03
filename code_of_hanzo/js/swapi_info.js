64e063309bbb4012dadc89a824bc9b174d922414
JasonLev <jasond.levy@gmail.com>
2017-07-03 09:26:27 -0700

    add wk27 exercises; swapi html and js refactoring

--- code_of_hanzo/js/swapi_info.js
+++ code_of_hanzo/js/swapi_info.js
@@ -1,5 +1,12 @@
 $(function() {
-  $.get("http://swapi.co/api/planets/1/", function (data) {
-    $("main").text("The planet of "+ data.name + " is a very " + data.climate + " place.")
-  });
+  $("form").submit(planetInfo);
+  function planetInfo(e) {
+    e.preventDefault();
+    var planet = $("select").val();
+    var url = "http://swapi.co/api/planets/"+ planet + "/"
+    $.get(url, function (data) {
+      console.log(data);
+      $("main").text("The planet of "+ data["name"] + " is a very " + data["climate"] + " place.")
+    });
+  }
 });
