70b12f67ce752b363a78c395c1a11364bd4a5aa8
JasonLev <jasond.levy@gmail.com>
2017-07-04 20:28:50 -0700

    continue swapi assignment

--- code_of_hanzo/js/swapi_info.js
+++ code_of_hanzo/js/swapi_info.js
@@ -1,11 +1,25 @@
 $(function() {
+  var page = 1;
+  var urlPlanets = "http://swapi.co/api/planets/?page=" + page;
+  loadPlanetOptions();
+  function loadPlanetOptions() {
+    $.get(urlPlanets, function (data) {
+      data.results.forEach(function (planet) {
+        var planetOption = $("<option></option>").attr("value", planet.url).text(planet.name);
+        $("select").append(planetOption);
+      });
+      // if $("option").length) < 62 {
+      //   $
+      // } else {
+      //
+      // };
+    });
+  }
   $("form").submit(planetInfo);
   function planetInfo(e) {
     e.preventDefault();
-    var planet = $("select").val();
-    var url = "http://swapi.co/api/planets/"+ planet + "/"
+    var url = $("select").val();
     $.get(url, function (data) {
-      console.log(data);
       $("main").text("The planet of "+ data["name"] + " is a very " + data["climate"] + " place.")
     });
   }
