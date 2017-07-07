7cb453f7394bf0fac5f4c6c84d9bcd2f3142ab50
JasonLev <jasond.levy@gmail.com>
2017-07-06 18:40:10 -0700

    swapi assignment refactor

--- code_of_hanzo/js/swapi_info.js
+++ code_of_hanzo/js/swapi_info.js
@@ -1,26 +1,30 @@
 $(function() {
   var page = 1;
-  var urlPlanets = "http://swapi.co/api/planets/?page=" + page;
-  loadPlanetOptions();
-  function loadPlanetOptions() {
+  loadPlanets();
+  function loadPlanets() {
+    var urlPlanets = "http://swapi.co/api/planets/?page=" + page;
     $.get(urlPlanets, function (data) {
       data.results.forEach(function (planet) {
-        var planetOption = $("<option></option>").attr("value", planet.url).text(planet.name);
-        $("select").append(planetOption);
+        var planetDiv = $("<div/>").text(planet.name)
+                                    .addClass("planet")
+                                    .click(function () {
+                                      $("main").text("The planet of "+ planet["name"] + " is a very " + planet["climate"] + " place.")
+                                    });
+        $(".planetContainer").append(planetDiv);
       });
-      // if $("option").length) < 62 {
-      //   $
-      // } else {
-      //
-      // };
+      addButton();
     });
   }
-  $("form").submit(planetInfo);
-  function planetInfo(e) {
-    e.preventDefault();
-    var url = $("select").val();
-    $.get(url, function (data) {
-      $("main").text("The planet of "+ data["name"] + " is a very " + data["climate"] + " place.")
-    });
+  function addButton() {
+    var planetCount = $(".planet").length;
+    if (planetCount < 61) {
+      $(".planetContainer").append(
+        $("<button type='button'>Add More Planets</button>").click(function () {
+          $(this).remove();
+          page++;
+          loadPlanets();
+        })
+      );
+    }
   }
 });
