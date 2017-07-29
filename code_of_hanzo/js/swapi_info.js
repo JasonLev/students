588a7416de81362deb1304985b7b1c1a6d57a145
JasonLev <jasond.levy@gmail.com>
2017-07-28 19:49:07 -0700

    swapi continued work to add more data and remove planets

--- code_of_hanzo/js/swapi_info.js
+++ code_of_hanzo/js/swapi_info.js
@@ -5,11 +5,29 @@ $(function() {
     var urlPlanets = "http://swapi.co/api/planets/?page=" + page;
     $.get(urlPlanets, function (data) {
       data.results.forEach(function (planet) {
+        var removeBtn = $("<button/>", {
+          "class": "removeBtn",
+          text: "X",
+          click: function () {
+            var removePlanet = ($(this).parent());
+            $(removePlanet).remove();
+          }
+        });
         var planetDiv = $("<div/>").text(planet.name)
                                    .addClass("planet")
-                                   .click(function () {
-                                      $("main").text("The planet of "+ planet["name"] + " is a very " + planet["climate"] + " place.")
-                                    });
+                                   .click(function (event) {
+                                     if (event.target.nodeName === "DIV") {
+                                       $("#planetFact1").text("The planet of "
+                                       + planet["name"]
+                                       + " is a very "
+                                       + planet["climate"]
+                                       + " place.");
+                                       $("#planetFact2").text("It has a population of "
+                                       + planet["population"]
+                                       + ".");
+                                     }
+                                   })
+                                   .append(removeBtn);
         $(".planetContainer").append(planetDiv);
       });
     }).then(
@@ -23,7 +41,7 @@ $(function() {
     var planetCount = $(".planet").length;
     if (planetCount < 61) {
       $(".planetContainer").append(
-        $("<button type='button'>Add More Planets</button>").click(function () {
+        $("<button type='button' class='addBtn'>Add More Planets</button>").click(function () {
           $(this).remove();
           page++;
           loadPlanets();
