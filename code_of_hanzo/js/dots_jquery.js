4fabfd605543ad4b925d5a7621d181c4a816b41b
JasonLev <jasond.levy@gmail.com>
2017-08-10 15:13:47 -0700

    add jquery to dots; change css of dots

--- code_of_hanzo/js/dots_jquery.js
+++ code_of_hanzo/js/dots_jquery.js
@@ -1,5 +1,28 @@
 $(function(){
-  $('input').change(function() {
-    $('body').css('backgroundColor', $(this).val());
-  })
+  $('#createDot').click(function() {
+    var dot = $("<div/>", {
+      "class": "dot",
+      click: function () {
+        $(this).remove();
+        checkForDots();
+      }
+    }).css("backgroundColor", $('input').val());
+    $('body').append(dot);
+    checkForDots();
+  });
+  function checkForDots() {
+    if ($('.dot').length) {
+      if ($("#changeDots").length == 0) {
+        $("<button/>", {
+          "id": "changeDots",
+          text: 'Change the color of all the dots',
+          click: function () {
+            $(".dot").css("backgroundColor", $('input').val());
+          }
+        }).appendTo($('.dotBtns'));
+      }
+    } else {
+      $("#changeDots").remove();
+    }
+  }
 });
