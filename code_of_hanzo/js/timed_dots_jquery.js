fd393446b4d8e2235350621a60cd9b674faf7022
JasonLev <jasond.levy@gmail.com>
2017-08-28 11:50:45 -0700

    add jquery starter code (ready for setInterval) to timed_dots exercise

--- code_of_hanzo/js/timed_dots_jquery.js
+++ code_of_hanzo/js/timed_dots_jquery.js
@@ -1,32 +1,40 @@
 $(function(){
   $('#createDot').click(function() {
-    var dot = $("<div/>", {
+    createDot();
+  });
+
+  function createDot() {
+    var $dot = $("<div/>",{
       "class": "dot",
       click: function () {
         $(this).remove();
         checkForDots();
-      }
-    }).css("backgroundColor", $('input').val());
-    //randomly set the "top" property
-    var top = Math.floor(Math.random() * (window.innerHeight - 205) + 125);
-    var left = Math.floor(Math.random() * (window.innerWidth - 75) + 25);
-
-    $('body').append(dot);
+      },
+      css: {
+        backgroundColor: $('input').val(),
+        top: Math.floor(Math.random() * (window.innerHeight - 305) + 200),
+        left: Math.floor(Math.random() * (window.innerWidth - 75) + 25)
+      },
+      appendTo: $('body')
+    });
     checkForDots();
-  });
+  };
+
   function checkForDots() {
     if ($('.dot').length) {
       if ($("#changeDots").length == 0) {
         $("<button/>", {
-          "id": "changeDots",
+          id: "changeDots",
           text: 'Change the color of all the dots',
           click: function () {
             $(".dot").css("backgroundColor", $('input').val());
-          }
-        }).appendTo($('.dotBtns'));
+          },
+          appendTo: $('.dotBtns')
+        });
       }
     } else {
       $("#changeDots").remove();
     }
   }
+
 });
