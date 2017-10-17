f7c4dd2c43bbca1866de509501156c77df9cf2ac
JasonLev <jasond.levy@gmail.com>
2017-10-17 11:46:51 -0700

    finish todo_list_jquery

--- code_of_hanzo/js/todo_list_jquery.js
+++ code_of_hanzo/js/todo_list_jquery.js
@@ -1,11 +1,23 @@
 $(function() {
+  function updateCounters() {
+    $("#total").text($('#todoList li').length);
+    $('#completed').text($('input:checked').length);
+    $('#remaining').text($('#total').text() - $('#completed').text());
+  }
   $('form').submit(function (event) {
     event.preventDefault();
-    if ($('#emptyList')) {
-      $('#emptyList').remove();
-    }
-    var todoItem = $('#newTodo').val().trim();
-    var todo = $('<li><label><input type="checkbox"><span>' + todoItem + '</span></label></li>');
-    todo.appendTo($('#todoList'));
-  })
+    var todoArr = $('#newTodo').val().split(",");
+    todoArr.forEach(function (task) {
+      if (task.trim()) {
+        if ($('#emptyList')) {
+          $('#emptyList').remove();
+        }
+        var todo = $('<li><label><input type="checkbox"><span>' + task.trim() + '</span></label></li>');
+        todo.appendTo($('#todoList'));
+        updateCounters();
+      }
+    })
+    $('#newTodo').val("");
+  });
+  $('#todoList').change(updateCounters);
 });
