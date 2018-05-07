fa81923301232fcb07c891eb75f3a72ec3568993
JasonLev <jasond.levy@gmail.com>
2018-05-07 12:52:35 -0700

    add shuffle and delayed card value to setup in memory_match

--- code_of_hanzo/js/memory_match.js
+++ code_of_hanzo/js/memory_match.js
@@ -1,3 +1,19 @@
+let collection = ["A",
+                  "A",
+                  "B",
+                  "B",
+                  "C",
+                  "C",
+                  "D",
+                  "D",
+                  "E",
+                  "E",
+                  "F",
+                  "F",
+                  "G",
+                  "G",
+                  "H",
+                  "H"];
 let cards = document.querySelectorAll(".card");
 cards.forEach(card => {
   card.addEventListener("click", () => {
@@ -9,14 +25,33 @@ let resetBtn = document.querySelector('button');
 let timer = document.querySelector('#timer');
 let intervalID;
 
-resetBtn.addEventListener("click", () => {
+resetBtn.addEventListener("click", startGame);
+
+function startGame() {
   resetBtn.textContent = "Restart Game";
-  timer.textContent = 0;
   clearInterval(intervalID);
+  timer.textContent = 0;
   intervalID = setInterval( () => {
     timer.textContent++;
   }, 1000);
+  setupCards();
+};
+
+function setupCards() {
+  //shuffle collection
+  for (let i = collection.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [collection[i], collection[j]] = [collection[j], collection[i]];
+  }
+  // flip all cards front-side up
   cards.forEach(card => {
     card.classList.remove('flipped');
   });
-});
+  //assign each card a value (after a delay to possibly let card flip to front)
+  let delayID;
+  delayID = setTimeout(() => {
+    cards.forEach( (card, index) => {
+      card.querySelector(".back span").textContent = collection[index];
+    });
+  }, 900);
+}
