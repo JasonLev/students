e665a3c6dc499ac050de6aed04d07537e1af786f
JasonLev <jasond.levy@gmail.com>
2018-05-07 11:26:12 -0700

    add timer and reset btn to memory_match; add topic for wk 53 to index

--- code_of_hanzo/js/memory_match.js
+++ code_of_hanzo/js/memory_match.js
@@ -1,7 +1,22 @@
 let cards = document.querySelectorAll(".card");
 cards.forEach(card => {
   card.addEventListener("click", () => {
-    console.log("card click");
     card.classList.toggle("flipped");
   }, false);
+});
+
+let resetBtn = document.querySelector('button');
+let timer = document.querySelector('#timer');
+let intervalID;
+
+resetBtn.addEventListener("click", () => {
+  resetBtn.textContent = "Restart Game";
+  timer.textContent = 0;
+  clearInterval(intervalID);
+  intervalID = setInterval( () => {
+    timer.textContent++;
+  }, 1000);
+  cards.forEach(card => {
+    card.classList.remove('flipped');
+  });
 });
