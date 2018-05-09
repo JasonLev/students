a8ce6257172ba81f0d3f8e6197d70ad99ec1a3a0
JasonLev <jasond.levy@gmail.com>
2018-05-08 22:31:21 -0700

    a_plus update to topics in index and border for box_model; memory_match with game logic

--- code_of_hanzo/js/memory_match.js
+++ code_of_hanzo/js/memory_match.js
@@ -14,27 +14,32 @@ let collection = ["A",
                   "G",
                   "H",
                   "H"];
+let matchVal = null;
 let cards = document.querySelectorAll(".card");
-cards.forEach(card => {
-  card.addEventListener("click", () => {
-    card.classList.toggle("flipped");
-  }, false);
-});
-
 let resetBtn = document.querySelector('button');
 let timer = document.querySelector('#timer');
+let gameStatus = document.querySelector('#game-status');
 let intervalID;
+let bestScore;
+
+cards.forEach(card => {
+  card.addEventListener("click", () => {
+    checkMatch(card)
+  }, false);
+});
 
 resetBtn.addEventListener("click", startGame);
 
 function startGame() {
   resetBtn.textContent = "Restart Game";
   clearInterval(intervalID);
+  matchVal = null;
   timer.textContent = 0;
+  gameStatus.textContent = "";
+  setupCards();
   intervalID = setInterval( () => {
     timer.textContent++;
   }, 1000);
-  setupCards();
 };
 
 function setupCards() {
@@ -45,13 +50,42 @@ function setupCards() {
   }
   // flip all cards front-side up
   cards.forEach(card => {
-    card.classList.remove('flipped');
+    card.classList.remove('flipped', 'open');
   });
   //assign each card a value (after a delay to possibly let card flip to front)
-  let delayID;
-  delayID = setTimeout(() => {
+  setTimeout(() => {
     cards.forEach( (card, index) => {
       card.querySelector(".back span").textContent = collection[index];
     });
   }, 900);
+}
+
+function checkMatch(card) {
+  let content = card.querySelector(".back span").textContent;
+  if (card.classList.contains("flipped") || content === "?????") {
+    return
+  } else if (matchVal === null) {
+    card.classList.add("flipped", "open");
+    matchVal = content;
+  } else if (matchVal === content) {
+    card.classList.add("flipped");
+    document.querySelector(".open").classList.remove("open");
+    matchVal = null;
+    checkGameOver();
+  } else {
+    let openCard = document.querySelector(".open");
+    card.classList.add("flipped");
+    openCard.classList.remove("open");
+    matchVal = null;
+    setTimeout(() => {
+      card.classList.remove("flipped");
+      openCard.classList.remove("flipped");
+    }, 1200);
+  }
+}
+function checkGameOver() {
+  if (document.querySelectorAll('.flipped').length === collection.length) {
+    clearInterval(intervalID);
+    gameStatus.textContent = "Game Over.  Great job!"
+  }
 }
